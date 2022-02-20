605b038bdfec901631213904e723d00df6b6addb
Pan <yeahpan@gmail.com>
2022-02-20 15:41:36 +0800

    test

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from re import S
 from flask import Flask, request, abort
 
 from linebot import (
@@ -37,9 +38,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    s = '8787'
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=s))
 
 
 if __name__ == "__main__":
