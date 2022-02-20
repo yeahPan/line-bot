70e04b59ccd5f0ab4a2968ff7a84b2d42a813e89
Pan <yeahpan@gmail.com>
2022-02-20 20:03:33 +0800

    update

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = '8787'
+    s = 'Hi 87'
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=s))
