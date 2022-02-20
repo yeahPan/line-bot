d395073ad04e0eeaf070df70bdb933a88273949e
Pan <yeahpan@gmail.com>
2022-02-20 21:32:21 +0800

    update

--- app.py
+++ app.py
@@ -44,15 +44,13 @@ def handle_message(event):
     elif msg in ['你是', '你是?', '妳是', '妳是?', '你是誰', '妳是誰', '你是誰?', '妳是誰?']:
         reply = '我是誰很重要嗎?'
     elif msg in ['白痴', '白癡', '白吃', '87']:
-        reply = '你才白痴!! 你全家都白痴!'
-
         sticker_message = StickerSendMessage(
         package_id='446',
         sticker_id='2019'
         )
         line_bot_api.reply_message(
         event.reply_token,
-        sticker_message, TextSendMessage(text=reply))
+        sticker_message)
 
     line_bot_api.reply_message(
         event.reply_token,
