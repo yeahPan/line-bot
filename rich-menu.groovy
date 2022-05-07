8a5cf19f6c46ddf3ea93ac258954645ca6aafbca
Pan <yeahpan@gmail.com>
2022-05-08 00:04:45 +0800

    update

--- rich-menu.groovy
+++ rich-menu.groovy
@@ -30,3 +30,6 @@ curl -v -X POST https://api.line.me/v2/bot/richmenu \
 {
   "richMenuId": "U057857240e6f1a0f143fff44e4f73569"
 }
+
+curl -v -X POST https://api.line.me/v2/bot/user/all/richmenu/richmenu-U057857240e6f1a0f143fff44e4f73569 \
+-H "Authorization: Bearer o8eopUX9WeQUMKcG/OuI0H+hxKKYDJ5dxW92GID+64P0Q0NiXZyS0Ugq0FK1RlhgOhdZZmGVf9bfSPNhEBVdzozi8C+xp6P1DBZV9zoEUo25b0SfqC+WAWYDdTTmbz4vlvoWI/Lrz41+GZu0melU7QdB04t89/1O/w1cDnyilFU="
